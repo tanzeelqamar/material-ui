3bdd9e9eb816f262bc9a2ba8df0afebc2516d885
Tanzeel Qamar <165699086+tanzeelqamar@users.noreply.github.com>
2025-09-30 21:10:21 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginSignup from './Component/LoginSignup';
+function App() {
+  return (
+    <div>
+      <LoginSignup />
+      
+    </div>
+  );
+}
+
+export default App;
